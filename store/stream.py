4d001a22ac3a1b4effd2a592f3fac9b0f5d54821
Antonin Delpeuch <antonin@delpeuch.eu>
2018-03-09 14:04:39 +0000

    Add edit stream

---
+++ store/stream.py
@@ -0,0 +1,19 @@
+import json
+from sseclient import SSEClient as EventSource
+
+class WikidataEditStream(object):
+    def __init__(self):
+        self.url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+        self.wiki = 'wikidatawiki'
+
+    def streamEdits(self):
+        for event in EventSource(self.url):
+            if event.event == 'message':
+                try:
+                    change = json.loads(event.data)
+                    if change.get('wiki') == self.wiki:
+                        yield change
+                except ValueError:
+                    pass
+
+
