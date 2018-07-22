3450125a16254c2971822fcf5f9d8d0ac47d9ca0
Antonin Delpeuch <antonin@delpeuch.eu>
2018-07-22 20:40:32 +0100

    Fix isadmin when user is not logged in

--- store/templatetags/isadmin.py
+++ store/templatetags/isadmin.py
@@ -1,11 +1,17 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
 @register.filter(is_safe=True)
 def isadmin(user):
-    socialauth = user.social_auth.get()
-    userinfo = socialauth.extra_data.get('userinfo', {})
-    return 'delete' in userinfo.get('rights', [])
+    if not user or not user.is_authenticated():
+        return False
+    try:
+        socialauth = user.social_auth.get()
+        userinfo = socialauth.extra_data.get('userinfo', {})
+        return 'delete' in userinfo.get('rights', [])
+    except ObjectDoesNotExist:
+        return False
 
