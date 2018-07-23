201b27cc62f084fd613119396066a84a49ab3a85
Antonin Delpeuch <antonin@delpeuch.eu>
2018-07-23 13:36:05 +0000

    Fix admin detection

--- store/templatetags/isadmin.py
+++ store/templatetags/isadmin.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 def isadmin(user):
-    if not user or not user.is_authenticated():
+    if not user or not user.is_authenticated:
         return False
     try:
         socialauth = user.social_auth.get()
