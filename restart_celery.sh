d979e4cd1f66a22ad41406135f916a4b1634596c
Antonin Delpeuch <antonin@delpeuch.eu>
2019-08-08 07:35:54 +0000

    GOMAXPROCS=1 in restart_celery.sh

--- restart_celery.sh
+++ restart_celery.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export GOMAXPROCS=1
 kubectl delete deployment editgroups.celery.sh ; kubectl create -f /data/project/editgroups/www/python/src/deployment/celery.yaml && kubectl get pods
