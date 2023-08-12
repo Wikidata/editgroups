31446f89043ca3fa46b3c24804b0fd15d41d8165
Antonin Delpeuch <antonin@delpeuch.eu>
2023-08-12 20:33:35 +0200

    Update to Django 4

--- tagging/models.py
+++ tagging/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from caching.base import CachingManager, CachingMixin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from store.models import Batch
 from collections import defaultdict
 
